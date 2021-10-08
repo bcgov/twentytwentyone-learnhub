8f326118637220c1797570b594525ac6c040eee3
Allan Haggett <allan.haggett@gov.bc.ca>
2021-10-07 19:10:50 -0700

    General tweaks to make it look better

--- template-parts/course/single-course.php
+++ template-parts/course/single-course.php
@@ -8,27 +8,29 @@
     <!-- <a href="#course-<?= $post->ID ?>" class="showdeets">#</a> -->
     </div>
     <div class="details" id="course-<?= $post->ID ?>">
-        <div class="learningpartner">
-            <?php the_terms( $post->ID, 'learning_partner', 'Offered by: ', ', ', ' ' ); ?>
-        </div>
-        <div class="coursedesc bg-white my-1">
+
+        <div class="coursedesc bg-white mt-wee">
             <?php the_content(); ?>
         </div>
-        <div class="coursecats">
-            <?php the_terms( $post->ID, 'course_category', 'Categories: ', ', ', ' ' ); ?>
+        <div class="learningpartner mt-wee">
+            <?php the_terms( $post->ID, 'learning_partner', 'Offered by: ', ', ', ' ' ); ?>
         </div>
-        <div class="delivery-method">
-            <?php the_terms( $post->ID, 'delivery_method', 'Delivery Method: ', ', ', ' ' ); ?>
-        </div>
-        <div class="courseregister">
+        <div class="courseregister my-1">
         <?php $exsys = get_the_terms( $post->ID, 'external_system', '', ', ', ' ' ) ?>
-        <a style="background: #28537d; color: #F2F2F2; font-size: 1.2rem; padding: .5em 1em; text-align: center; text-decoration: none;" 
+        <a class="registerbutton" 
             href="<?= $post->course_link ?>" 
             target="_blank" 
             rel="noopener">
                 Register on <?= $exsys[0]->name ?>
         </a>
         </div>
+        <div class="coursecats mt-1">
+            <?php the_terms( $post->ID, 'course_category', 'Categories: ', ', ', ' ' ); ?>
+        </div>
+        <div class="delivery-method">
+            <?php the_terms( $post->ID, 'delivery_method', 'Delivery Method: ', ', ', ' ' ); ?>
+        </div>
+
     </div>
     </div>
 </div> <!-- /.course -->
