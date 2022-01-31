a2bc4314906401667a3c13b4ed7ac85f2b04fc01
Allan Haggett <allan.haggett@gov.bc.ca>
2022-01-31 15:19:38 -0800

    No link or system checks

--- template-parts/course/single-course.php
+++ template-parts/course/single-course.php
@@ -19,19 +19,29 @@
             <?php the_terms( $post->ID, 'learning_partner', 'Offered by: ', ', ', ' ' ); ?>
         </div>
 
-
         <div class="courseregister mt-1">
+        <?php if(!empty($post->course_link)): ?>
+        
         <?php $exsys = get_the_terms( $post->ID, 'external_system', '', ', ', ' ' ) ?>
         <a class="registerbutton" 
             href="<?= $post->course_link ?>" 
             target="_blank" 
             rel="noopener">
+                <?php if(!empty($exsys[0]->name)): ?>
                 Register on <?= $exsys[0]->name ?>
+                <?php else: ?>
+                Get More Information
+                <?php endif ?>
         </a>
+
+        <?php else: ?>
+
+            <div>Oh no! There's something wrong with this course. Please <?php the_terms( $post->ID, 'learning_partner', 'contact', ', ', ' ' ); ?></div>
+
+        <?php endif ?>
         </div>
 
 
-        
         <div class="coursecats mt-1" style="display:none;">
             <?php the_terms( $post->ID, 'course_category', 'Categories: ', ', ', ' ' ); ?>
         </div>
