98eadcf5ceeadd2703f20f157d4628ca28d8416b
Nori Sinclair <Nori.Sinclair@gov.bc.ca>
2024-04-03 23:46:59 +0000

    add bootstrap css and js, work on nav

--- front-page.php
+++ front-page.php
@@ -23,10 +23,7 @@ endwhile; // End of the loop.
         <div class="wp-block-columns alignwide is-layout-flex wp-container-core-columns-layout-1 wp-block-columns-is-layout-flex">
             <div class="wp-block-column is-layout-flow wp-block-column-is-layout-flow">
                 <h2 class="wp-block-heading has-extra-large-font-size">What is the LearningHUB?</h2>
-                <p>In the B.C. Public Service, corporate learning is a shared space.
-                    The Learning Centre and its partners offer hundreds of courses,
-                    available to all BCPS employees. The Learning Hub is the place
-                    to see that full catalogue.</p>
+                <p>In the B.C. Public Service, corporate learning is a shared space. The Learning Centre and its partners offer hundreds of courses, available to all BCPS employees. The Learning Hub is the place to see that full catalogue.</p>
             </div>
             <div class="wp-block-column is-layout-flow wp-block-column-is-layout-flow" style="padding-top:var(--wp--preset--spacing--40);padding-right:var(--wp--preset--spacing--40);padding-bottom:var(--wp--preset--spacing--40);padding-left:var(--wp--preset--spacing--40)" id="homesearchbox">
                 <form role="search" method="get" action="https://wordpress.virtuallearn.ca/learninghub/" class="wp-block-search__button-outside wp-block-search__text-button wp-block-search">
@@ -36,108 +33,103 @@ endwhile; // End of the loop.
                         <button aria-label="Search" class="wp-block-search__button has-text-color has-white-color wp-element-button" type="submit">Search</button>
                     </div>
                 </form>
-                <div ><a href="/learninghub/?s=flexibleBCPS">#flexibleBCPS</a> | <a href="/learninghub/?s=BCPSBelonging">#BCPSBelonging</a></div>
+                <div>Suggested: <a href="/learninghub/?s=flexibleBCPS">#flexibleBCPS</a> <a href="/learninghub/?s=BCPSBelonging">#BCPSBelonging</a></div>
             </div>
         </div>
     </div>
 </div>
-<div class="alignwide">
-    <h3>How is learning organized?</h3>
-    <p>Four types of categorization help you find exactly what you’re looking for:
-        group, topic, audience and delivery.</p>
-    <div style="background-color: #FFF; border-radius: .5em; margin: 1em 0; padding: 1em;">
-        <h4 class="alignwide" style="flex:none">Groups</h4>
-        <div class="wp-block-columns alignfull">
-            <?php
-            $groups = get_terms(array(
-                'taxonomy' => 'groups',
-                'hide_empty' => false,
-                'orderby'    => 'count',
-                'order'   => 'DESC'
-            ));
+<div class="alignwide" style="margin-top: 1em;">
+    <div class="has-white-background-color" style="padding: 1em;">
+        <h3>How is learning organized?</h3>
+        <p>Four types of categorization help you find exactly what you're looking for:
+            group, audience, topic and delivery. </p>
+        <p style="color: gray;"><small>Click on any category link for search results filtered by that term.</small></p>
 
-            ?>
-            <?php foreach ($groups as $g) : ?>
-                <div class="wp-block-column" style="padding: 1em;">
+
+        <div class="wp-block-columns alignfull" style="margin-top: 1em; gap: 1em;">
+            <div class="wp-block-column">
+                <h4 class="alignwide" style="flex:none">Group</h4>
+                <?php
+                $groups = get_terms(array(
+                    'taxonomy' => 'groups',
+                    'hide_empty' => false,
+                    'orderby'    => 'count',
+                    'order'   => 'DESC'
+                ));
+
+                ?>
+                <?php foreach ($groups as $g) : ?>
+
                     <a href="/learninghub/groups/<?= $g->slug ?>" style="text-decoration: none">
-                        <div class="groupicon group-<?= $g->slug ?>" style="color: #003366;"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
-                                <path style="color: #003366;" d="M448 256A192 192 0 1 0 64 256a192 192 0 1 0 384 0zM0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256zm256 80a80 80 0 1 0 0-160 80 80 0 1 0 0 160zm0-224a144 144 0 1 1 0 288 144 144 0 1 1 0-288zM224 256a32 32 0 1 1 64 0 32 32 0 1 1 -64 0z" />
-                            </svg></div>
-                        <div><span style="font-weight: bold; text-decoration: underline;"><?= $g->name ?></span></div>
-                        <div><?= $g->description ?></div>
+                        <!-- <div class="groupicon group-<?= $g->slug ?>" style="color: #003366;"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">-->
+                        <!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
+                        <!--<path style="color: #003366;" d="M448 256A192 192 0 1 0 64 256a192 192 0 1 0 384 0zM0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256zm256 80a80 80 0 1 0 0-160 80 80 0 1 0 0 160zm0-224a144 144 0 1 1 0 288 144 144 0 1 1 0-288zM224 256a32 32 0 1 1 64 0 32 32 0 1 1 -64 0z" />
+                            </svg></div> -->
+                        <div><span style="font-weight: bold; text-decoration: underline;"><?= $g->name ?></span>: <?= $g->description ?></div>
                     </a>
-                </div>
-            <?php endforeach ?>
 
-        </div>
-    </div>
-    <div class="wp-block-columns alignfull">
-        <div class="wp-block-column" style="background-color: #FFF; border-radius: .5em; padding: 1em;">
-            <h4 class="alignwide">Topics</h4>
-            <?php
-            $topics = get_terms(array(
-                'taxonomy' => 'topics',
-                'hide_empty' => false,
-                'orderby'    => 'count',
-                'order'   => 'DESC'
-            )); // 121 = Office of Compt General, 372 = unknown, 144 = labour relations 
-
-            ?>
-            <div style="columns:2 auto">
-                <?php foreach ($topics as $t) : ?>
-                    <div><a style="font-weight: bold;" href="/learninghub/topics/<?= $t->slug ?>"><?= $t->name ?></a></div>
                 <?php endforeach ?>
             </div>
-        </div>
-    </div>
-    <div class="wp-block-columns alignfull"><!-- wp:column -->
-        <div class="wp-block-column">
-            <?php
-            $audiences = get_terms(array(
-                'taxonomy' => 'audience',
-                'hide_empty' => false,
-                'orderby'    => 'count',
-                'order'   => 'DESC'
-            )); // 121 = Office of Compt General, 372 = unknown, 144 = labour relations 
-
-            ?>
             <div class="wp-block-column">
-                <div style="background-color: #FFF; border-radius: .5em; margin-right: 1em; margin-top: 0; padding: 1em;">
-                    <h4>Audiences</h4>
-                    <?php foreach ($audiences as $a) : ?>
-                        <div class="" style="">
-                            <div>
-                                <a style="font-weight: bold;" href="/learninghub/audience/<?= $a->slug ?>"><?= $a->name ?></a>:
-                                <?= $a->description ?>
-                            </div>
-                        </div>
-                    <?php endforeach ?>
-                </div>
-            </div>
-        </div>
-        <div class="wp-block-column">
-            <div style="background-color: #FFF; border-radius: .5em; padding: 1em;">
-                <h4>Delivery Method</h4>
                 <?php
-                $delivery = get_terms(array(
-                    'taxonomy' => 'delivery_method',
+                $audiences = get_terms(array(
+                    'taxonomy' => 'audience',
                     'hide_empty' => false,
                     'orderby'    => 'count',
                     'order'   => 'DESC'
                 )); // 121 = Office of Compt General, 372 = unknown, 144 = labour relations 
 
                 ?>
-                <?php foreach ($delivery as $d) : ?>
+                <h4>Audience</h4>
+                <?php foreach ($audiences as $a) : ?>
                     <div class="" style="">
                         <div>
-                            <a style="font-weight: bold;" href="/learninghub/delivery_method/<?= $d->slug ?>"><?= $d->name ?></a>:
-                            <?= $d->description ?>
+                            <a style="font-weight: bold;" href="/learninghub/audience/<?= $a->slug ?>"><?= $a->name ?></a>:
+                            <?= $a->description ?>
                         </div>
                     </div>
-
                 <?php endforeach ?>
             </div>
-        </div>
-    </div>
 
-    <?php get_footer() ?>
+        </div>
+        <div style="background-color: #FFF; border-radius: .5em; padding: 1em;">
+            <h4>Delivery Method</h4>
+            <?php
+            $delivery = get_terms(array(
+                'taxonomy' => 'delivery_method',
+                'hide_empty' => false,
+                'orderby'    => 'count',
+                'order'   => 'DESC'
+            )); // 121 = Office of Compt General, 372 = unknown, 144 = labour relations 
+
+            ?>
+            <?php foreach ($delivery as $d) : ?>
+                <div class="" style="">
+                    <div>
+                        <a style="font-weight: bold;" href="/learninghub/delivery_method/<?= $d->slug ?>"><?= $d->name ?></a>:
+                        <?= $d->description ?>
+                    </div>
+                </div>
+
+            <?php endforeach ?>
+        </div>
+        <div class="wp-block-columns alignfull">
+            <div class="wp-block-column" style="background-color: #FFF; border-radius: .5em; padding: 1em;">
+                <h4 class="alignwide">Topics</h4>
+                <?php
+                $topics = get_terms(array(
+                    'taxonomy' => 'topics',
+                    'hide_empty' => false,
+                    'orderby'    => 'count',
+                    'order'   => 'DESC'
+                )); // 121 = Office of Compt General, 372 = unknown, 144 = labour relations 
+
+                ?>
+                <div style="columns:2 auto">
+                    <?php foreach ($topics as $t) : ?>
+                        <div><a style="font-weight: bold;" href="/learninghub/topics/<?= $t->slug ?>"><?= $t->name ?></a></div>
+                    <?php endforeach ?>
+                </div>
+            </div>
+        </div>
+        <?php get_footer() ?>
--- template-parts/header/site-header.php
+++ template-parts/header/site-header.php
@@ -16,45 +16,59 @@ $blog_info    = get_bloginfo('name');
 ?>
 
 <div id="bcgovmast">
-    <header id="mainheader" class="mainhead" role="banner">
-        <div class="alignwide">
+    <header id="mainheader" role="banner">
+        <nav class="navbar navbar-expand-lg px-3" data-bs-theme="dark" role="navigation" aria-label="<?php esc_attr_e('Primary menu', 'twentytwentyone'); ?>">
+            <div class="container-fluid">
+                <a href="<?php echo esc_url(home_url('/')); ?>" class="wordmark navbar-brand">
+                    <?php //the_custom_logo(); 
+                    ?>
+                    Learning<span style="color: #ebba44; font-weight: bold;">HUB</span>
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <!-- <li class="nav-item">
+                            <a class="nav-link" aria-current="page" href="/learninghub/">Home</a>
+                        </li> -->
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="/learninghub/about/" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                About</a>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="#">About the LearningHUB</a></li>
+                                <li><a class="dropdown-item" href="/learninghub/corporate-learning-partners/">Learning Partners</a></li>
+                                <li><a class="dropdown-item" href="/learninghub/what-is-corp-learning-framework/">Corporate Learning Framework</a></li>
+                            </ul>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="/learninghub/learning-systems/" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Learning Systems</a>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="https://learning.gov.bc.ca/CHIPSPLM/signon.html">PSA Learning System </a></li>
+                                <li><a class="dropdown-item" href="https://learningcurator.gww.gov.bc.ca/">Learning Curator</a></li>
+                                <li><a class="dropdown-item" href="/learninghub/learning-systems/">All Learning Systems</a></li>
+                            </ul>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/learninghub/course/">All Courses</a>
+                        </li>
+                    </ul>
 
-
-            <div class="headwrap">
-                <div style="flex: 0;">
-                    <a href="<?php echo esc_url(home_url('/')); ?>" class="wordmark">
-                        <?php //the_custom_logo(); 
-                        ?>
-                        Learning<span style="color: #ebba44; font-weight: bold;">HUB</span>
-                    </a>
+                    <form method="get" action="/learninghub/" class="d-flex" role="search">
+                        <label for="s" class="sr-only">Search</label>
+                        <input type="search" id="s" class="s bg-white" name="s" placeholder="Find learning" required value="<?= esc_html(get_search_query()) ?>">
+                        <button type="submit" class="searchsubmit" aria-label="Submit Search" style="font-size: 18px">
+                            Search
+                        </button>
+                    </form>
                 </div>
-
-                <div style="flex: 2 0;">
-                    <nav class="main-nav" style="display: flex; align-items: center;justify-content: flex-end;" role="navigation" aria-label="<?php esc_attr_e('Primary menu', 'twentytwentyone'); ?>">
-                        <div>
-                            <!-- <a href="/learninghub/">Home</a> -->
-                            <a href="/learninghub/about/">About</a>
-                            <a href="/learninghub/corporate-learning-partners/">Partners</a>
-                            <a href="/learninghub/learning-systems/">Learning Systems</a>
-                            <a href="/learninghub/course/">All Courses</a>
-                        </div>
-                    </nav><!-- #site-navigation -->
-                </div>
-                <form method="get" action="/learninghub/" class="searchform" style="flex:0 1; max-width: 40vw;">
-                    <label for="s" class="sr-only" style="">Search</label>
-                    <input type="search" id="s" class="s" name="s" placeholder="What do you want to learn?" required value="<?= esc_html(get_search_query()) ?>">
-                    <button type="submit" class="searchsubmit" aria-label="Submit Search" style="font-size: 18px">
-                        Search
-                    </button>
-                </form>
             </div>
-        </div>
+        </nav>
+
+
+
+
 
     </header><!-- #masthead -->
-</div>
-<div id="searchbar" style="background-color: #01284f;">
-    <div class="alignwide"> <!-- searchwrap -->
-
-        <div style="clear:both"></div>
-    </div>
 </div>
